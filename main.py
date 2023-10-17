1393ed90a60e873af5b16534b0d85d0f8627e42a
Oliver Funk <oli.funk@gmail.com>
2023-10-17 11:49:36 +0100

    small change

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from pprint import pprint
 
 from eqdsk.file import EQDSKInterface
 
-EQDSKInterface.DEFAULT_COCOS_CONVENTION = 7
-ed = EQDSKInterface.from_file("eudemo.eqdsk")
+# to test, can change the default convention like this
+# EQDSKInterface.DEFAULT_COCOS_CONVENTION = 7
+ed = EQDSKInterface.from_file("test_eqdsks/eudemo.eqdsk")
 
 pprint(ed.cocos_convention)
