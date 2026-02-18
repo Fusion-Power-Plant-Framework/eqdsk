2741309b39b64bc776115d0c070c0bbf8b0a0115
Timothy <75321887+timothy-nunn@users.noreply.github.com>
2026-02-18 14:21:23 +0000

    Test IMAS HDF5 backend (#117)

--- tests/test_imas.py
+++ tests/test_imas.py
@@ -18,6 +18,16 @@ else:
     from imas import DBEntry
 
 
+@pytest.fixture
+def _imas_netcdf_uri(tmp_path):
+    return tmp_path / "test.nc"
+
+
+@pytest.fixture
+def _imas_hdf5_uri(tmp_path):
+    return f"imas:hdf5?path={(tmp_path / 'imasdb').as_posix()}"
+
+
 @pytest.mark.parametrize(
     ("file", "cocos"),
     [("jetto.eqdsk_out", "jetto"), ("DN-DEMO_eqref_withCoilNames.json", 3)],
@@ -32,18 +42,21 @@ else:
     ],
 )
 @pytest.mark.parametrize("file_format", ["imas", None])
+@pytest.mark.parametrize("imas_uri_fixture", ["_imas_netcdf_uri", "_imas_hdf5_uri"])
 def test_imas_write_read(
-    tmp_path, file, cocos, imas_dd_version, coil_comparison, file_format
+    imas_uri_fixture, file, cocos, imas_dd_version, coil_comparison, file_format, request
 ):
     """Test an eqdsk file can be read and then written to IMAS"""
+    imas_uri = request.getfixturevalue(imas_uri_fixture)
+
     eqdsk = EQDSKInterface.from_file(
         DATA_DIR / file, from_cocos=cocos, to_cocos=11, qpsi_positive=False
     )
 
-    with DBEntry(tmp_path / "test.nc", "w", dd_version=imas_dd_version) as db:
+    with DBEntry(imas_uri, "w", dd_version=imas_dd_version) as db:
         eqdsk.write(db, file_format=file_format)
 
-    with DBEntry(tmp_path / "test.nc", "r", dd_version=imas_dd_version) as db:
+    with DBEntry(imas_uri, "r", dd_version=imas_dd_version) as db:
         new_eqdsk = EQDSKInterface.from_imas(db).to_cocos(11)
 
     # IMAS has no defined mechanism for storing coil types
