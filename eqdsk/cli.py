c0d01e2f6ca1f7c53d7efd2c2decab170e795dd8
Timothy Nunn <timothy.nunn@ukaea.uk>
2026-02-18 13:49:11 +0000

    Assume default imas conversion output is NetCDF

--- eqdsk/cli.py
+++ eqdsk/cli.py
@@ -356,7 +356,7 @@ def convert(  # noqa: PLR0913, PLR0917
 
         if uri is None:
             uri = (
-                Path(filepath_or_uri)
+                Path(filepath_or_uri.replace("imas:hdf5?path=", ""))
                 .with_stem(f"{Path(filepath_or_uri).stem}_out")
                 .as_posix()
                 + ".nc"
