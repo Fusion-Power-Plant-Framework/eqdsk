20378f254e2818f13097700aaf3aefb46ba1cd17
Jonathan Matthews <jonathan.matthews@ukaea.uk>
2026-04-21 11:56:15 +0100

    fix to cocos.py file to work with new changes to identify and altered the test usage of qpsi_positive so is depenedent on cocos of input

--- eqdsk/cocos.py
+++ eqdsk/cocos.py
@@ -15,6 +15,7 @@ from typing import TYPE_CHECKING, Any
 import numpy as np
 
 from eqdsk.models import Sign, ZeroOne
+from eqdsk.log import eqdsk_warn
 
 if TYPE_CHECKING:
     import numpy.typing as npt
