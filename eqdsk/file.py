8611f17b45725d91fa9c2997ae9b025cee48cd0b
athoy.nilima@ukaea.uk <athoy.nilima@ukaea.uk>
2026-06-01 08:25:29 +0100

    Revert "Output COCOS to eqdsk if with_comment=True"
    This reverts commit d049ba04cceb453e579db3271bd618e2e3bc91d6.

--- eqdsk/file.py
+++ eqdsk/file.py
@@ -536,10 +536,6 @@ Grid properties:
         del d["unprocessed_data"]
         if not with_comment:
             d.pop("comment")
-        elif "comment" in d and d["comment"] is not None:
-            d["comment"] += f"; cocos: {self.cocos.index}"
-        else:
-            d["comment"] = f"cocos: {self.cocos.index}"
 
         return d
 
@@ -622,10 +618,6 @@ Grid properties:
             5e16.9, disabling this changes the format to 5ES23.16e2
         write_comment:
             write any comments to file (is not considered when writing to IMAS).
-            if True, the comment will be written to file and the COCOS index will
-            be appended to any existing comment.
-            if False and cocos index is already identified, the comment will only
-            include the COCOS index.
 
         Raises
         ------
--- tests/test_file.py
+++ tests/test_file.py
@@ -352,17 +352,15 @@ class TestEQDSKInterface:
             with path.open() as file:
                 f_data = json.load(file)
             if comment:
-                assert f_data["comment"] == extra[1:] + f"; cocos: {eqdsk.cocos.index}"
+                assert f_data["comment"] == extra[1:]
             else:
                 assert f_data.get("comment") is None
 
         elif ftype == "eqdsk":
             f_data = path.read_text()
             if comment:
-                assert f_data.endswith(f"\n\n{extra}; cocos: {eqdsk.cocos.index}\n")
-                assert not f_data.endswith(
-                    f"\n\n\n{extra}; cocos: {eqdsk.cocos.index}\n"
-                )
+                assert f_data.endswith(f"\n\n{extra}\n")
+                assert not f_data.endswith(f"\n\n\n{extra}\n")
             else:
                 assert not f_data.endswith(extra)
 
